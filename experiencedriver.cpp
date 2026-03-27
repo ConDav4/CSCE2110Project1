437f2a0c5c73d348a71846329c0de3ddcd0eb4c7
DuncanWrenn <duncanwrenn@gmail.com>
2026-03-27 11:07:16 -0500

    experiencedriver.cpp

--- experiencedriver.cpp
+++ experiencedriver.cpp
@@ -1,6 +1,11 @@
 #include "experiencedriver.h"
 using namespace std;
-
+ExperienceDriver::ExperienceDriver(){
+	expCat = "NULL";
+}
+ExperienceDriver::~ExperienceDriver(){
+	delete expCat;
+}
 void ExperienceDriver::setExpCat(string e) {
     expCat = e;
 }
