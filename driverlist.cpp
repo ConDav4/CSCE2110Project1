1e725d5055fe187ab1b8d00048a5802961bd2400
DuncanWrenn <duncanwrenn@gmail.com>
2026-03-27 20:21:45 -0500

    driverlist.cpp
    -accidentally replaced with address.cpp???
    -fixed a bit of my logic from before

--- driverlist.cpp
+++ driverlist.cpp
@@ -1,58 +1,145 @@
-#include "address.h"
+#include "driverlist.h"
+#include <iostream>
+#include <string>
 using namespace std;
 
-Address::Address() {
-    street = "NULL";
-    city = "NULL";
-    county = "NULL";
-    state = "NULL";
-    zip = -1;
+DriverList::DriverList(){
+	head = nullptr;
+	tail = nullptr;
+	size = 0;
+}
+DriverList::~DriverList(){
+	Node* cur = head;
+	while(cur) {
+		Node* next = cur->next;
+		delete cur;
+		cur = next;
+	}
 }
 
-Address::Address(string str, string ci, string co, string sta, int z) {
-    street = str;
-    city = ci;
-    county = co;
-    state = sta;
-    zip = z;
+void DriverList::insertAtHead(Driver* d){
+	Node* n = new Node(d);
+	if(!head){
+		head = tail = n;
+	} else {
+		n->next = head;
+		head->prev = n;
+		head = n;
+	}
+	++size;
 }
 
-void Address::setStreet(string str) {
-    street = str;
+void DriverList::insertAtTail(Driver* d) {
+	Node* n = new Node(d);
+	if(!tail) {
+		head = tail = n;
+	} else {
+		tail->next = n;
+		n->prev = tail;
+		tail = n;
+	}
+	++size;
 }
 
-string Address::getStreet() const {
-    return street;
+void DriverList::insertAtMiddle(Driver* d) {
+	if(!head || !head->next) {
+		insertAtTail(d);
+		return;
+	}
+	int mid = size / 2;
+	Node* cur = head;
+	for(int i=0; i<mid && cur; ++i) {
+		cur = cur->next;
+	}
+	if(!cur){
+		insertAtTail(d);
+		return;
+	}
+	Node* n = new Node(d);
+	n->next = cur->next;
+	n->prev = cur;
+	if(cur->next) cur->next->prev = n;
+	cur->next = n;
+	if (cur == tail) {
+		tail = n;
+	}
+	++size;
 }
 
-void Address::setCity(string ci) {
-    city = ci;
+void DriverList::insertByCounty(Driver* d, string co) {
+	if(!head) {
+		insertAtHead(d);
+		return;
+	}
+	Node* cur = head;
+	while(cur) {
+		if(cur->data && cur->data->getAddress().getCounty() == co) {
+			Node * n = new Node(d);
+			n->next = cur->next;
+			n->prev = cur;
+			if (cur->next) cur->next->prev = n;
+			else tail=n;
+			cur->next = n;
+			++size;
+			return;
+		}
+		cur = cur->next;
+	}
+	if(!cur){
+		cout<<"No matching county: put at end."<<endl;
+		insertAtTail(d);
+	}
 }
 
-string Address::getCity() const {
-    return city;
+void DriverList::deleteDriver(int DriverID) {
+	string id = to_string(driverID);
+	Node* cur = head;
+	while(cur) {
+		if (cur->data && cur->data->getDriverID() == id) {
+			if(cur->prev) {
+				cur->prev->next = cur->next;
+			} else {
+				head = cur->next;
+			}
+		if (cur->next) {
+			cur->next->prev = cur->prev;
+		} else {
+			tail = cur->prev;
+		}
+		delete cur;
+		--size;
+		return;
+		}
+		cur = cur-> next;
+	}
 }
 
-void Address::setCounty(string co) {
-    county = co;
+Driver* DriverList::searchDriver(int driverID) {
+	string id = to_string(driverID);
+	Node* cur = head;
+	while(cur) {
+		if (cur->data && cur->data->getDriverID() == id) {
+			return cur->data;
+		}
+		cur = cur->next;
+	}
+	return nullptr;
 }
 
-string Address::getCounty() const {
-    return county;
+void DriverList::printAll() const {
+	Node* cur = head;
+	while (cur) {
+		if(cur->data) {
+			cur->data->displayDriver();
+		}
+		cur = cur -> next;
+	}
 }
 
-void Address::setState(string sta) {
-    state = sta;
+Node* DriverList::getHead() const {
+	return head;
 }
 
-string Address::getState() const {
-    return state;
-}
-
-void Address::setZIP(int z) {
-    zip = z;
-}
-
-int Address::getZIP() const {
-    return zip;
+int DriverList::getSize() const {
+	return size;
 }
